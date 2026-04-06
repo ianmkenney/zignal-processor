430e1731c409d032d8e87a7b7f4b4613e1482ab9
Ian Kenney <ianmichaelkenney@gmail.com>
2026-04-06 13:22:58 -0400

    Sample generator with generics

--- src/main.zig
+++ src/main.zig
@@ -4,15 +4,21 @@ const Complex = std.math.Complex;
 const Signal = @import("data.zig").Signal;
 const Spectrum = @import("data.zig").Spectrum;
 
-fn sample_signal(t: f32) Complex(f32) {
-    return .init(std.math.sin(5 * t) + std.math.cos(7 * t) + 3, 0);
+fn sample_signal(comptime T: type) (fn (T) Complex(T)) {
+    const inner = struct {
+        fn _sample_signal(t: T) Complex(T) {
+            return .init(std.math.sin(5.0 * t) + std.math.cos(9.0 * t) + 1.0, 0);
+        }
+    };
+
+    return inner._sample_signal;
 }
 
 pub fn main() !void {
     const n_samples = 32;
-    const precision = f32;
+    const precision = f64;
 
-    var samples = Signal(precision, n_samples).initFn(sample_signal);
+    var samples = Signal(precision, n_samples).initFn(sample_signal(precision));
     var spectrum = samples.dft();
     var recon = spectrum.idft();
 
