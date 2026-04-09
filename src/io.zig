2f665c3cc4ee8f48ccacbf55ebc6b5d45feaf720
Ian Kenney <ianmichaelkenney@gmail.com>
2026-04-09 14:54:24 -0400

    Write to binary file

--- src/io.zig
+++ src/io.zig
@@ -7,21 +7,18 @@ const Complex = std.math.Complex;
 /// 1. Data index
 /// 2. Real component
 /// 3. Complex component
-pub fn write_complex(comptime T: type, filename: []const u8, signal: []const Complex(T)) !void {
-    const file = try std.fs.cwd().createFile(
-        filename,
-        .{ .read = true },
-    );
+pub fn write_complex(
+    comptime T: type,
+    filename: []const u8,
+    signal: []const Complex(T),
+) !void {
+    const file = try std.fs.cwd().createFile(filename, .{});
     defer file.close();
 
     for (signal, 0..) |data, i| {
-        const string = try std.fmt.allocPrint(
-            std.heap.page_allocator,
-            "{}\t{}\t{}\n",
-            .{ i, data.re, data.im },
-        );
-        defer std.heap.page_allocator.free(string);
-        _ = try file.write(string);
+        _ = try file.write(std.mem.asBytes(&i));
+        _ = try file.write(std.mem.asBytes(&data.re));
+        _ = try file.write(std.mem.asBytes(&data.im));
     }
 }
 
@@ -36,11 +33,12 @@ pub fn read(comptime T: type, filename: []const u8, allocator: std.mem.Allocator
     var file_reader = file.reader(&file_buffer);
 
     var array: std.ArrayList(Complex(T)) = .empty;
-    while (try file_reader.interface.takeDelimiter('\n')) |line| {
-        var field_it = std.mem.splitScalar(u8, line, '\t');
-        _ = field_it.next();
-        const re = try std.fmt.parseFloat(T, field_it.next().?);
-        const im = try std.fmt.parseFloat(T, field_it.next().?);
+
+    const width = 2 * @sizeOf(T) + @sizeOf(usize);
+
+    while (file_reader.interface.take(width) catch null) |line| {
+        const re = std.mem.bytesToValue(T, line[@sizeOf(usize) .. @sizeOf(usize) + @sizeOf(T)]);
+        const im = std.mem.bytesToValue(T, line[@sizeOf(usize) + @sizeOf(T) ..]);
         const value: std.math.Complex(T) = .init(re, im);
         try array.append(allocator, value);
     }
